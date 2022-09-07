bb068f364c028396b91b581ba064037ae546b3dd
andyvhuynh01 <andyvhuynh@gmail.com>
2022-09-07 01:41:26 -0500

    :sparkles: FEAT Home page header

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,22 @@ import './header.css'
 function Header() {
     return (
         <div className='header'>
-            <div className ='headerInfo'>
-                <img className='profile' src={ require('../img/profile.jpg') } />
-                <div>
-                    <div style={{'font-size': '30px', 'font-weight': 'bold'}}>Hello there, I'm Andy!</div>
-                    <div style={{'font-style': 'italic', 'padding-bottom': '20px'}}>I'm discovering software and biology.</div>
-                    <div>Evolution Research Intern @ The Petrov Lab</div>
-                    <div>Computer Science Student @ Stanford University</div>
+            <div className ='infoBox'>
+                <img className='profileImg' src={ require('../img/profile.jpg') } />
+                <div className='infoBoxRight'>
+                    <div>
+                        <div className='textName'>Hello there, I'm Andy!</div>
+                        <div className='textSlogan'>I'm discovering software and biology.</div>
+                    </div>
+                    <div>
+                        <div className='textRole'>🧬 Evolution Research Intern @ The Petrov Lab</div>
+                        <div className='textRole'>💻 Computer Science Student @ Stanford University</div>
+                    </div>
+                    <div className='socialBox'>
+                        <img className='socialIcon' src={require('../img/linkedin.png')}/>
+                        <img className='socialIcon' src={require('../img/github.png')}/>
+                        <img className='socialIcon' src={require('../img/email.png')}/>
+                    </div>
                 </div>
             </div>
         </div>
