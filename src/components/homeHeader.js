22b4f6fe3e9e898edf318ed88b4977f2795b0a94
Andy Huynh <andyhuynh@Andys-MBP-2.lan>
2023-12-23 15:38:12 -0600

    redesign, simplified

--- src/components/homeHeader.js
+++ src/components/homeHeader.js
@@ -7,18 +7,19 @@ function Header() {
     return (
         <div className='header'>
             <div className ='infoBox'>
-                <img className='profileImg' src={ require('../img/profile.png')} />
+                <img className='profileImg' src={ require('../img/profile.jpeg')} />
                 <div className='infoBoxRight'>
                     <div>
                         <div className='textName'>Hello there, I'm Andy!</div>
-                        <div className='textSlogan'>I'm curious about using machine learning for sustainability.</div>
+                        <div style={{'font-style': 'italic'}}>I'm a final-year CS MS student and researcher—in computer vision and biology—curious about using ML for biodiversity.</div>
+                        {/* <div className='textSlogan'>I'm curious about using machine learning for sustainability.</div> */}
                     </div>
                     <div>
                         <Link className='socialButton' to='#' onClick={() => {window.open('https://www.moisesexpositoalonso.org/')}}>
-                            <div className='textRole'>🌱 Research Assistant @ Carnegie Institute for Science</div>
+                            <div className='textRole'>🌱 Graduate Researcher @ Carnegie Institute for Science</div>
                         </Link>
                         <Link className='socialButton' to='#' onClick={() => {window.open('https://cs.stanford.edu/')}}>
-                            <div className='textRole'>💻 M.S. & B.S. Computer Science @ Stanford University</div>
+                            <div className='textRole'>💻 MS & BS Computer Science @ Stanford University</div>
                         </Link>
                     </div>
                 </div>
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -29,63 +29,81 @@ function HomePage() {
                     ✨ <span style={{color: '#335381', 'font-weight': 'bold'}}>  Seeking a software engineering internship for summer 2023. </span>
                 </div>
             </div> */}
-            <div className='heading'>Research — Machine Learning</div>
+            <div className='heading'>Research</div>
             <div style={{width: '30%', borderTop: '2px solid #446491', marginBottom: '30px'}}></div>
             <div className='projGallery'>
                 <div className='project'>
-                    <img className='projImg' src={ require('../../img/thumbnails/crisp.jpg')} />
+                    <img className='projImg' src={ require('../../img/thumbnails/crisp.png')} />
                     <div className='projTxt'> 
-                        <div className='projTitle'>From CLIP to CRISP: Contrastive Remote Sensing-Image Pretraining using Weakly Labeled Data</div>
-                        <div className='projAuthors'>Andy V. Huynh, Rohan Sikand, Jael Lopez-Saucedo, Claire Tang, Lauren Gillespie</div>
-                        <div className='projDesc'>CS 197 Computer Science Research Project</div>
-                        <div className='projLinks'>Paper | Presentation</div>
+                        <div className='projTitle'>Contrastive ground-level image and remote sensing pre-training improves representation learning for natural world imagery</div>
+                        {/* <div className='projAuthors'>Andy V. Huynh, Rohan Sikand, Jael Lopez-Saucedo, Claire Tang, Lauren Gillespie</div> */}
+                        {/* <div className='projDesc'>CS 197 Computer Science Research Project</div> */}
+                        <div className='projLinks'>
+                            <a href="https://drive.google.com/file/d/1GdZ8Dyc8F0FxG722sBi_4crn4nHVOCIw/view" target="_blank" rel="noopener noreferrer">presentation</a>&nbsp; | &nbsp;
+                            <a href="https://github.com/moiexpositoalonsolab/crisp-private" target="_blank" rel="noopener noreferrer">code</a>
+                        </div>
                     </div>
                 </div>
                 <div className='project'>
-                    <div className='projImg'></div>
+                    <img className='projImg' src={ require('../../img/thumbnails/census.png')} />
                     <div className='projTxt'> 
-                        <div className='projTitle'>Exploring Unsupervised Methods for Acoustic Bat Species Classification and Individual Identification</div>
-                        <div className='projAuthors'>Andy V. Huynh, Andreas Paepcke</div>
-                        <div className='projDesc'>CS 191W Writing-Intensive Independent Study</div>
-                        <div className='projLinks'>Paper | Presentation</div>
-                    </div>
-                </div>
-            </div>
-            <div className='heading' style={{marginTop: "50px"}}>Research — Biology</div>
-            <div style={{width: '30%', borderTop: '2px solid #446491', marginBottom: '30px'}}></div>
-            <div className='projGallery'>
-                <div className='project'>
-                    <img className='projImg'/>
-                    <div className='projTxt'> 
-                        <div className='projTitle'>Adaptive tracking in response to insecticide pressure in Drosophila</div>
-                        <div className='projAuthors'>Andy V. Huynh, Marianthi Karageorgi, Mark C. Bitter, Caitlynn To-Duyen Tran, Dmitri A. Petrov</div>
-                        <div className='projDesc'>Petrov Lab, Dept. of Biology, Stanford Univeristy</div>
-                        <div className='projLinks'>Poster</div>
+                        <div className='projTitle'>Exploring automated methods for drosophila census estimation + Adaptive tracking in response to insecticide pressure in Drosophila</div>
+                        {/* <div className='projAuthors'>Andy V. Huynh, Andreas Paepcke</div> */}
+                        {/* <div className='projDesc'>CS 191W Writing-Intensive Independent Study</div> */}
+                        <div className='projLinks'>
+                            <a href="https://github.com/codebyandy/drosophila-census/blob/main/meeting_slides.pdf">slides</a>&nbsp; | &nbsp; 
+                            <a href="https://drive.google.com/file/d/1pJ3B66c8V80N1IAPZV-PMTDdZc2Wh5Uy/view?usp=sharing">poster</a>&nbsp; | &nbsp;
+                            <a href="https://www.biorxiv.org/content/10.1101/2023.10.16.562586v1">paper</a>&nbsp; | &nbsp; 
+                            <a href="https://github.com/codebyandy/drosophila-census">code</a>
+                        </div>
                     </div>
                 </div>
                 <div className='project'>
-                    <div className='projImg'></div>
+                    <img className='projImg' src={ require('../../img/thumbnails/rrv.png')} />
                     <div className='projTxt'> 
-                        <div className='projTitle'>How Climate Drives Mosquito-Borne Diseases in Queensland</div>
-                        <div className='projAuthors'>Andy Huynh, Lisa Couper, Eloise Skinner, Erin Mordecai</div>
-                        <div className='projDesc'>Mordecai Lab, Dept. of Biology, Stanford Univeristy</div>
-                        <div className='projLinks'>Poster</div>
+                        <div className='projTitle'>How climate drives Ross River Virus and Barmah Forest Virus in Queensland, Australia</div>
+                        {/* <div className='projAuthors'>Andy V. Huynh, Andreas Paepcke</div> */}
+                        {/* <div className='projDesc'>CS 191W Writing-Intensive Independent Study</div> */}
+                        <div className='projLinks'>
+                            <a href="https://drive.google.com/file/d/1Buq0ofCHShL4k2CRSBwBfwcFQWxJrvtV/view?usp=sharing">poster</a>
+                        </div>
                     </div>
                 </div>
                 <div className='project'>
-                    <div className='projImg'></div>
+                    <img className='projImg' src={ require('../../img/thumbnails/insulin.png')} />
                     <div className='projTxt'> 
-                        <div className='projTitle'>Mechanisms of Action of Synthetic Inhibitors of Insulin Secretion</div>
-                        <div className='projAuthors'>Andy V. Huynh, Rohan Sikand, Jael Lopez-Saucedo, Claire Tang, Lauren Gillespie</div>
-                        <div className='projDesc'>CS 197 Computer Science Research Project</div>
-                        <div className='projLinks'>Paper | Presentation | Code</div>
-                    </div>
-                </div>
-            </div>
-            <div className='heading' style={{marginTop: "50px"}}>Other Activities</div>
-            <div style={{width: '30%', borderTop: '2px solid #446491', marginBottom: '30px'}}></div>
-            </div>
+                        <div className='projTitle'>Small molecule-mediated insulin hypersecretion induces transient ER stress response and loss of beta cell function</div>
+                        {/* <div className='projAuthors'>Andy V. Huynh, Andreas Paepcke</div> */}
+                        {/* <div className='projDesc'>CS 191W Writing-Intensive Independent Study</div> */}
+                        <div className='multiLinks'>
+                            <a href="https://pubmed.ncbi.nlm.nih.gov/35641126/" target="_blank" rel="noopener noreferrer">paper</a>&nbsp; | &nbsp; 
+                            <a href="https://drive.google.com/file/d/11Tz0xUeuYw3y4TwstQspqJlcQC_LN5zf/view?usp=drive_link" target="_blank" rel="noopener noreferrer">poster</a>&nbsp; | &nbsp; 
+                            <a href="https://drive.google.com/file/d/1e8yiKTzyp-Fjm-V5n4PcR4J4LJ4tNjsI/view?usp=drive_link" target="_blank" rel="noopener noreferrer">presentation</a>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
+            <div className='heading' style={{marginTop: "50px"}}>Other</div>
+            <div style={{width: '30%', borderTop: '2px solid #446491', marginBottom: '30px'}}></div>
+            <div>
+                <Link className='socialButton' to='#' onClick={() => {window.open('https://resed.stanford.edu/neighborhoods/aspen/aspen-houses/larkin')}}>
+                    <div className='textRole'>🏠 Resident Assistant @ Larkin House</div>
+                </Link>
+                <Link className='socialButton' to='#' onClick={() => {window.open('https://jrbp.stanford.edu/')}}>
+                    <div className='textRole'>🌳 Docent @ Jasper Ridge Biological Preserve</div>
+                </Link>
+                <Link className='socialButton' to='#' onClick={() => {window.open('https://codebyandy.github.io/portfolio')}}>
+                    <div className='textRole'>📸 Wildlife photographer</div>
+                </Link>
+                {/* <Link className='socialButton' to='#' onClick={() => {window.open('https://www.moisesexpositoalonso.org/')}}>
+                    <div className='textRole'>🗞️ (ex) Managing Editor of Photo & Video @ The Stanford Daily</div>
+                </Link>
+                <Link className='socialButton' to='#' onClick={() => {window.open('https://www.moisesexpositoalonso.org/')}}>
+                    <div className='textRole'>📚 (ex) Education Partnerships Fellow @ The High School Support Initiative</div>
+                </Link> */}
+            </div>
+            </div>
         </>
     )
 }
